59701263f755e393b1eaa933ca1abca11a32a5ef
PolinaBasova <126002275+PolinaBasova@users.noreply.github.com>
2025-03-11 21:14:06 +0700

    Update task1.c

--- task1.c
+++ task1.c
@@ -5,6 +5,7 @@
 #include <TH1D.h>
 #include <TF1.h>
 #include <TCanvas.h>
+#include "TGraph.h"
 
 using namespace std;
 
@@ -19,51 +20,49 @@ void t1(){
         //deviation from the mean = 25000/500 for uniform distribution
         Dev = fabs(graph->GetBinContent(i) - 50); 
         //for gauss sigma=sqrt(mean)
-        if (Dev > sqrt(50)) StDev1+=1;// 500*0.32 = 160  
-        if (Dev > 2*sqrt(50)) StDev2+=1; //500*0.05 = 25
-        if (Dev > 3*sqrt(50)) StDev3+=1; //500*0.003 = 1.5
+        if (Dev > sqrt(50)) StDev1+=1;// 100*0.32  
+        if (Dev > 2*sqrt(50)) StDev2+=1; //100*0.05 
+        if (Dev > 3*sqrt(50)) StDev3+=1; //100*0.003 
     }
     cout << StDev1 << "\n";
     cout << StDev2 << "\n";
     cout << StDev3 << "\n";
-    s->SaveAs("task1.png");
+    s->SaveAs("Task1.png");
 
     int N = 10;
-    auto f1 = new TF1("f1","TMath::Gaus(x, [mean], [sigma], 1) ", 0, 20);
-    f1->SetNpx(100000);
-    f1->SetParameter("mean", N);
-    f1->SetParameter("sigma",sqrt(N));
-    auto f2 = new TF1("f2","TMath::Poisson(x, [mean]) ", 0, 20);
-    f2->SetNpx(100000);
-    f2->SetParameter("mean", N);
-    f1 -> GetHistogram() -> Draw();
-    f2 -> SetLineColor(kGreen);
-    f2 -> GetHistogram() -> Draw("same");
-    s-> SaveAs("task2.png");
+    auto Gaus1 = new TF1("Gaus1","TMath::Gaus(x, [mean], [sigma], 1) ", 0, 200);
+    Gaus1->SetNpx(100000);
+    Gaus1->SetParameter("mean", N);
+    Gaus1->SetParameter("sigma",sqrt(N));
+    auto Poisson1 = new TF1("Poisson1","TMath::Poisson(x, [mean]) ", 0, 200);
+    Poisson1->SetNpx(100000);
+    Poisson1->SetParameter("mean", N);
+    Gaus1 -> GetHistogram() -> Draw();
+    Poisson1 -> SetLineColor(kGreen);
+    Poisson1 -> GetHistogram() -> Draw("same");
+    s-> SaveAs("Task1Gauss&&Poisson.png");
 
-    
-    auto f3 = new TF1("f3","10*TMath::Binomial([n], x) * ([p])^x * (1-[p])^([n]-x) ", 0, 100);
-    auto f4 = new TF1("f2","TMath::Poisson(x, [mean]) ", 0, 100);
-    f3->SetNpx(100); f4->SetNpx(100);
-    f3->SetParameter("n", 1000);
-    f3->SetParameter("p", 0.01);
-    f4->SetParameter("mean", 10);
-    f3 -> GetHistogram() -> Draw();
-    f4 -> SetLineColor(kGreen);
-    f4 -> GetHistogram() -> Draw("same");
-    s-> SaveAs("task3.png");
+    N = 100;
+    auto Binom2 = new TF1("Binom2","ROOT::Math::binomial_pdf( x, [p], [n])", 0, 100);
+    auto Poisson2 = new TF1("Poisson2","TMath::Poisson(x, [mean]) ", 0, 100);
+    Binom2->SetNpx(100); Poisson2->SetNpx(100);
+    Binom2->SetParameter("n", 1000);
+    Binom2->SetParameter("p", 0.01);
+    Poisson2->SetParameter("mean", 10);
+    Binom2 -> GetHistogram() -> Draw();
+    Poisson2 -> SetLineColor(kGreen);
+    Poisson2 -> GetHistogram() -> Draw("same");
+    s-> SaveAs("Task1Binom&&Poisson.png");
 
-    N = 10;
-    double norm = TMath::Gamma(N/2)*TMath::Power(2.,N/2);
-    auto f5 = new TF1("f5","pow(x,[N]/2-1)*exp(-0.5*x)/[norm]", 0, 100);
-    f5->SetParameter("N", N);
-    f5->SetParameter("norm", norm);
-    auto f6 = new TF1("f6","TMath::Gaus(x, [mean], [sigma], 1)", 0, 100);
-    f6->SetParameter("mean", N);
-    f6->SetParameter("sigma",sqrt(2*N));
-    f6 -> GetHistogram() -> Draw();
-    f5 -> SetLineColor(kGreen);
-    f5 -> GetHistogram() -> Draw("same");
-    s-> SaveAs("task4.png");
+    N = 50;
+    auto ChiSq3 = new TF1("ChiSq3","ROOT::Math::chisquared_pdf(x,[N])", 0, 200);
+    ChiSq3->SetParameter("N", N);
+    auto Gaus3 = new TF1("Gaus3","TMath::Gaus(x, [mean], [sigma], 1)", 0, 200);
+    Gaus3->SetParameter("mean", N);
+    Gaus3->SetParameter("sigma",sqrt(2*N));
+    Gaus3 -> GetHistogram() -> Draw();
+    ChiSq3 -> SetLineColor(kGreen);
+    ChiSq3 -> GetHistogram() -> Draw("same");
+    s-> SaveAs("Task1Chisq&&Gaus.png");
 
 }
